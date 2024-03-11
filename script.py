059b7923f335ec10b40901897a77300f77ad310a
sjouide <sjouide@gitlab.bsc.es>
2024-03-11 22:31:49 +0100

    .

--- script.py
+++ script.py
@@ -20,18 +20,29 @@ if response_get.status_code == 200:
     # Get the content of the file from the GET response
     file_content = response_get.content
 
+    # Save the content to a file within the execution folder
+    execution_folder_path = os.path.join(user_id, execution_id)
+    os.makedirs(execution_folder_path, exist_ok=True)
+    #file_path = os.path.join(execution_folder_path, 'summary.txt')
+    file_path = 'summary.txt'
+
+
+    with open(file_path, 'wb') as file:
+        file.write(file_content)
+
+
     # Construct the POST URL with dataset ID, user ID, and execution ID
-    post_url = f"{post_base_url}/{file_id}?user_id={user_id}&execution_id={execution_id}"
+    #post_url = f"{post_base_url}/{file_id}?user_id={user_id}&execution_id={execution_id}"
 
     # Perform POST request with the entire file content and basic authentication
-    files = {'files': ('file_content.txt', file_content)}
-    auth = (username, password)
-    response_post = requests.post(post_url, files=files, auth=auth)
+    #files = {'files': ('file_content.txt', file_content)}
+    #auth = (username, password)
+    #response_post = requests.post(post_url, files=files, auth=auth)
 
     # Print response information
     print("GET Request Status Code:", response_get.status_code)
-    print("POST Request Status Code:", response_post.status_code)
-    print("POST Response Text:", response_post.text)
+    #print("POST Request Status Code:", response_post.status_code)
+    #print("POST Response Text:", response_post.text)
 else:
     print(f"GET Request failed with status code {response_get.status_code}")
 
